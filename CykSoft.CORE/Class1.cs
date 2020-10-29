e558127f3bbbc23023e2cc09d8031bcba73120bf
Mateo Betancur <mateo-beta@hotmail.com>
2020-10-28 20:58:03 -0500

    Proyecto inicial

---
+++ CykSoft.CORE/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CykSoft.CORE
+{
+    public class Class1
+    {
+    }
+}
---
+++ CykSoft.INFRASTRUCTURE/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CykSoft.INFRASTRUCTURE
+{
+    public class Class1
+    {
+    }
+}
